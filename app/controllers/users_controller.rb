51229c7519fbd051f41d1af2c5f4b435e427f520
Aaron Chu <aaronchu357@gmail.com>
2019-07-25 11:45:38 -0400

    added user info in addition to user payload upon create in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,10 @@ class UsersController < ApplicationController
     if user.valid?
       if params[:national_dish_attributes]
         params[:national_dish_attributes].each do |national_dish_param|
-          @user.national_dish << NationalDish.find(national_dish_param[:id])
+          user.national_dish << NationalDish.find(national_dish_param[:id])
         end
       end
-      render json: { token: encode_token(user_payload(user)) }, status: :created
+      render json: { token: encode_token(user_payload(user)), user: user }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
