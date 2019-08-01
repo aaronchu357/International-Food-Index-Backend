78246c2df51b7b7a846bec20cf8778e7bb1dadf3
Aaron Chu <aaronchu357@gmail.com>
2019-07-31 20:00:25 -0400

    updated comments controller with UD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,24 @@ class CommentsController < ApplicationController
     render json: CommentSerializer.new(comment)
   end
 
+  # PATCH/PUT /recipes/1
+  def update
+    comment = Comment.find(params[:id])
+    if comment.update(comment_params)
+      render json: CommentSerializer.new(comment)
+    else
+      render json: comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /recipes/1
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+  end
+
   private
+
   def comment_params
     params.permit(:content, :rating, :user_id, :national_dish_id)
   end
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,6 +1,6 @@
 class CommentSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :content, :rating, :id, :user
+  attributes :content, :rating, :id, :user, :national_dish
   belongs_to :user
   belongs_to :national_dish
 end
